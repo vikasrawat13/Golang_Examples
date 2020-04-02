e2490b6af71110175499abe2a34ad2f12b6c480f
vrawat4 <vrawat.reply@gmail.com>
2020-04-02 21:49:59 +0530

    new files & packages created

--- src/main.go
+++ src/main.go
@@ -2,23 +2,35 @@ package main
 
 import (
 	"fmt"
-	p "pkgs"
+	t "topics"
 	s "stringops"
 )
 
 func main() {
+	var arrData [10]int
+
 	fmt.Printf("%c", 95)
 	//calling all functions exists under pkgs
-	p.SampleFunc()
-	p.TypesofLoops()
-	p.IfElseFunc()
-	fmt.Println("Reverse of Vikas is", p.StrRev("vikas"))
+	t.SampleFunc()
+	t.TypesofLoops()
+	t.IfElseFunc()
+	fmt.Println("Reverse of Vikas is", t.StrRev("vikas"))
 	fmt.Println(s.StrReverse("vikas"))
 	fmt.Println(int('a'))
 
 	fmt.Println("-----------------------------------------")
-	x := ""
+	x := "1AbT%34d1038dms#$@&^$"
 	fmt.Println(s.GetAlpha(x))
 	fmt.Println(s.GetNumbers(x))
 	fmt.Println(s.GetSpecialChar(x))
+	dArrays(&arrData)
+	for _, value := range arrData {
+		fmt.Println(value)
+	}
+}
+
+func dArrays(arr *[10]int) {
+	for i := 0; i < 10; i++ {
+		(*arr)[i] = i
+	}
 }
---
+++ src/topics/varDeclaration.go
@@ -0,0 +1,65 @@
+package topics
+
+import "fmt"
+
+// variable declarations
+var x int = 50
+
+// variable declaration
+var (
+	y    = 50
+	z    = 100
+	name = `vikas rawat`
+	a    = 10.4
+	m    = true
+)
+
+/* %T represent variable Type
+   %d represent integer
+   %f represent float
+   %t represent boolean
+   %s represent string
+   %v represent default variable type format
+   %b represent binary
+   %c reprsent character code
+*/
+
+// variable declarations
+const ax int = 1022
+
+// variable declarations
+var a1, a2 int = 20, 30
+var d = 43
+var abc string
+
+//SampleFunc user defined function to print different variables
+func SampleFunc() {
+
+	// variable declaration
+	msg := "hello world"
+	display := fmt.Println
+
+	if x > 10 {
+		display("x is greater than 10")
+	} else {
+		fmt.Println("x is smaller than 10")
+	}
+
+	display(msg)
+	conversion(&msg)
+	display(msg)
+	fmt.Printf("value of ax is %v", ax)
+	fmt.Printf("\nvalue of a is %.2f", a) // handle decimal precision
+	display("\nHello " + ", I'm Vikas Rawat")
+	fmt.Printf("%c", 33)
+}
+
+func conversion(x *string) {
+	*x = "HELLO WORLD.!"
+	//return x
+}
+
+/*// reverse a string
+func strReverse(x string)string{
+	for
+}*/
